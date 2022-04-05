d86acbcd8ae2a18aa561f890a614e5c50fce0f31
Герман Кабачков <german_kabachkov@mail.ru>
2022-04-05 19:22:41 +0300

    -_-

---
+++ yatube/posts/views.py
@@ -0,0 +1,154 @@
+from django.shortcuts import redirect, render, get_object_or_404
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+
+from .models import Post, Group, Comment, Follow, User
+from .forms import CommentForm, PostForm
+
+NUMBERS_OF_POST = 10
+
+
+def index(request):
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    title = 'Последние обновления на сайте'
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'title': title,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    post_list = group.posts.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    post_list = Post.objects.filter(author=author).order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    post_count = post_list.filter(author_id=author).count()
+    title = f'Профайл пользователя {author}'
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    following = False
+    if request.user.is_authenticated:
+        if Follow.objects.filter(author=author, user=request.user).exists():
+            following = True
+    context = {
+        'title': title,
+        'page_obj': page_obj,
+        'author': author,
+        'post_count': post_count,
+        'following': following
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    comments = Comment.objects.filter(post=post).order_by('-created')
+    form = CommentForm(request.POST or None)
+    author = post.author
+    post_count = Post.objects.filter(author=author).count()
+    context = {
+        'post': post,
+        'author': author,
+        'post_count': post_count,
+        'form': form,
+        'comments': comments
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', username=post.author)
+    return render(request, 'posts/create_post.html', {'form': form})
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.user != post.author:
+        return redirect('posts:post_detail', post_id=post_id)
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=post,
+
+    )
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id=post_id)
+    context = {
+        'form': form,
+        'is_edit': True,
+    }
+    return render(request, 'posts/create_post.html', context)
+
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+    return redirect('posts:post_detail', post_id=post_id)
+
+
+@login_required
+def follow_index(request):
+    following = Follow.objects.filter(user=request.user)
+    follow_list = []
+    for follow in following:
+        follow_list.append(follow.author)
+    post_list = Post.objects.filter(
+        author__in=follow_list
+    ).order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    title = 'Ваши подписки'
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'title': title,
+        'page_obj': page_obj
+    }
+    return render(request, 'posts/follow.html', context)
+
+
+@login_required
+def profile_follow(request, username):
+    author = get_object_or_404(User, username=username)
+    user = request.user
+    if user != author and not Follow.objects.filter(author=author, user=request.user).exists():
+        Follow.objects.create(user=user, author=author)
+    return redirect('posts:profile', username=username)
+
+
+@login_required
+def profile_unfollow(request, username):
+    author = get_object_or_404(User, username=username)
+    user = request.user
+    Follow.objects.filter(user=user, author=author).delete()
+    return redirect('posts:profile', username=username)
